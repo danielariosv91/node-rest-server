ac91310e4e7e05b2835454709b786ec22acbc178
Daniela <daniela.rios.v91@gmail.com>
2019-03-10 20:01:32 -0300

    agregar conexion

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDatabase;
 if (process.env.NODE_ENV === 'dev') {
     urlDatabase = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDatabase = 'mongodb+srv://danibiicha:ZjjAimN4US5aFZh@cluster0-1q7ty.mongodb.net/cafe';
+    urlDatabase = 'mongodb+srv://danibiicha:ZjjAimN4US5aFZh@cluster0-smoju.mongodb.net/cafe';
 }
 
 process.env.URL_DB = urlDatabase;
